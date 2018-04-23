8f2ab37e122e92cee45d2c2f3148fc3f83a0f8a2
Cedric Reichmuths <cedric.reichmuth@gamil.com>
2018-04-23 08:09:44 -0400

    fixed wrong display of cart-image for screen width 600-1024px

--- js/index.js
+++ js/index.js
@@ -38,8 +38,8 @@ $(function() {
     event.preventDefault();
     var email = $('#email').val(),
       emailReg = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  if(!emailReg.test(email) || email == '') {
-    alert('Please enter a valid email address.');
+    if(!emailReg.test(email) || email == '') {
+      alert('Please enter a valid email address.');
        return false;
   } else {
       alert('Thanks for subscribing!');
